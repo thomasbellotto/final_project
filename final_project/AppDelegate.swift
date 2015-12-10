a80c524910b43803fcb1554bb818e6960ce8574f
Thomas Silva <thomas_thoti@hotmail.com>
2015-12-10 00:27:14 -0500

    iOS Project - First

--- final_project/AppDelegate.swift
+++ final_project/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.setApplicationId("LPhfzfjiHR30GbYyt5RP2aWgxOYwYjPmFCwlBsAS", clientKey:"uDQnSEgGROSaez366Sk4DQlC0AlCc4Jg3z10OeQy")
         return true
     }
 
--- final_project/ViewController.swift
+++ final_project/ViewController.swift
@@ -7,19 +7,87 @@
 //
 
 import UIKit
+import Parse
+import ParseUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
 
+    var logInViewController: PFLogInViewController! = PFLogInViewController()
+    var signUpViewController: PFSignUpViewController! = PFSignUpViewController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+
+        super.viewDidAppear(animated)
+        
+        if(PFUser.currentUser() == nil){
+            
+            self.logInViewController.fields = [.UsernameAndPassword, .LogInButton, .SignUpButton, .PasswordForgotten, .DismissButton]
+            
+            let logInLogoTitle = UILabel()
+            logInLogoTitle.text = "Parse iOS Log IN"
+            self.logInViewController.logInView!.logo = logInLogoTitle
+            self.logInViewController.delegate = self
+            
+            
+            let signUpLogoTitle = UILabel()
+            signUpLogoTitle.text = "Parse iOS Sign UP"
+            self.signUpViewController.signUpView!.logo = signUpLogoTitle
+            self.logInViewController.signUpController = self.signUpViewController
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    /************** Parse Login ******************/
+    
+    func logInViewController(logInController: PFLogInViewController, shouldBeginLogInWithUsername username: String, password: String) -> Bool {
+        
+        if(!username.isEmpty || !password.isEmpty){
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func logInViewController(logInController: PFLogInViewController, didFailToLogInWithError error: NSError?) {
+        print("Fail to Log in")
+    }
+    
+    /************* Parse Sign Up *****************/
+    
+    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func signUpViewController(signUpController: PFSignUpViewController, didFailToSignUpWithError error: NSError?) {
+        print("Fail to Sign Up")
+    }
+    
+    func signUpViewControllerDidCancelSignUp(signUpController: PFSignUpViewController) {
+        print("User dismissed sign up.")
+        
+    }
+    
+    /************ Actions *********************/
+    
+    /*@IBAction func logoutAction(sender: AnyObject) {
+        PFUser.logOut()
+    }*/
 
 }
 
